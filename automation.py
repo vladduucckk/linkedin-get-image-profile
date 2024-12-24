bcf75af61ed9d4b82fe1b2abfa4cf045c5d02ace
vladduucckk <vladislavmojseev@gmail.com>
2024-12-24 16:18:41 +0200

    add condition for create directory with images

--- automation.py
+++ automation.py
@@ -7,11 +7,13 @@ import requests
 
 from dotenv import load_dotenv
 
+if os.path.isdir(f"{os.getcwd()}/img-profile") is False:
+    os.system('mkdir img-profile')
+
 load_dotenv()
 
 LOGIN_LINKEDIN = os.getenv('LOGIN_LINKEDIN')
 PASSWORD_LINKEDIN = os.getenv('PASSWORD_LINKEDIN')
-
 
 url = 'https://www.linkedin.com/login/ru?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin'
 
@@ -22,7 +24,7 @@ driver.get(url)
 
 login_field = driver.find_element(By.ID, 'username')
 password_field = driver.find_element(By.ID, 'password')
-button_click = driver.find_element(By.XPATH,'//button[@type="submit"]')
+button_click = driver.find_element(By.XPATH, '//button[@type="submit"]')
 
 login_field.send_keys(LOGIN_LINKEDIN)
 password_field.send_keys(PASSWORD_LINKEDIN)
@@ -30,8 +32,8 @@ button_click.click()
 
 tab_with_img = driver.current_window_handle
 driver.switch_to.window(tab_with_img)
-img_profile_src = driver.find_element(By.XPATH,'//a[@class="ember-view block"]//img').get_attribute('src')
+img_profile_src = driver.find_element(By.XPATH, '//a[@class="ember-view block"]//img').get_attribute('src')
 response = requests.get(img_profile_src)
 
-with open('profile_image.png', 'wb') as f:
+with open('img-profile/profile_image.png', 'wb') as f:
     f.write(response.content)
